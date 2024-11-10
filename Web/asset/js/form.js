3d53bbdf25a19b9371ae16e084fa526dd9b05f97
ndyah2020 <nguyenduyanhh2020@gmail.com>
2024-11-10 14:10:39 +0700

    checkInputs

--- Web/asset/js/form.js
+++ Web/asset/js/form.js
@@ -9,11 +9,17 @@ let DataUsers = localStorage.getItem('DataUsers') ? JSON.parse(localStorage.getI
         cartItems: [
         ],
     },
+    {
+        id:         2,
+        isAdmin:    0,
+        name: "",
+        email:      'client@gmail.com',
+        password:   '123123',
+        cartItems: [
+        ],
+    },
 ];
 
-if (!localStorage.getItem("DataUsers")) {
-    localStorage.setItem("DataUsers", JSON.stringify(DataUsers));
-}
 //Hiển thị login khi ấn vào icon
 const showLoginAndRegister = () => {
     const showLogin = () => loginModal.classList.add('open');
@@ -49,7 +55,104 @@ const showForm = () => {
         })
     }) 
 }
+
+const checkInputs = () => {
+    
+    function Validator(options) {
+
+        const validate = (inputElement, rule) => {
+
+            const errorElement = inputElement.parentElement.parentElement.querySelector(options.errorSelector);
+            const errorMessage = rule.test(inputElement.value);
+
+            if (errorMessage) {
+
+                errorElement.innerText = errorMessage;
+                inputElement.classList.add('errorInput');
+            } else {
+
+                errorElement.innerText = '';
+                inputElement.classList.remove('errorInput');
+            }
+        
+            inputElement.addEventListener('input', () => {
+                
+                errorElement.innerText = '';
+                inputElement.classList.remove('errorInput');    
+            });
+        };
+
+        const formElement = document.querySelector(options.form)
+        if(formElement){
+    
+            options.rules.forEach((rule) => {
+
+                const inputEmlement = formElement.querySelector(rule.selector)
+                
+                if(inputEmlement){
+                    inputEmlement.addEventListener('blur', () => 
+                        validate(inputEmlement,rule)
+                    ) 
+                }
+    
+            })
+        }
+    }
+    
+    //Kiểm tra có nhập vào dữ liệu không
+    Validator.isRequired = (selector, message) => ({
+        selector: selector,
+        test: (value) => value.trim() ? undefined : message || "Vui lòng nhập trường này"
+    })
+
+    //Kiểm tra định dạng email
+    Validator.isEmail = (selector, message) => ({
+        selector: selector,
+        test: (value) => {
+            const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : message || "Trường này phải là email"
+        }
+    })
+    //Kiểm tra password phải trên 6 ký tự
+    Validator.isPassword = (selector, min, message) => ({
+        selector: selector,
+        test: (value) => value.length >= min ? undefined : message || `Password phải ${min} trở lên`
+    })
+    //Kiểm tra password Comfime phải giống với password
+    Validator.isComfimer = (selector, getComfirmValue, message) => ({
+        selector: selector,
+        test: (value) => value === getComfirmValue() ? undefined : message || 'Giá trị nhập vào không trùng khớp'
+    })
+
+    //Kiểm tra input đăng ký
+    Validator({
+        form: '#form-2',
+        errorSelector: '.form-message',
+        rules: [
+            Validator.isRequired('#name'),
+            Validator.isEmail('#email2'),
+            Validator.isPassword('#password2', 6),
+            Validator.isComfimer(
+                '#password_confirmation', 
+                () => document.querySelector('#form-2 #password2').value, 
+                'Mật khẩu không trùng khớp'
+            )
+        ]
+    })
+    //Kiểm tra input đăng nhập
+    Validator({
+        form: '#form-1',
+        errorSelector: '.form-message',
+        rules: [
+            Validator.isEmail('#email1'),
+            Validator.isPassword('#password1'),
+        ]
+    })
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     showForm();
     showLoginAndRegister();
+    checkInputs();
 })
